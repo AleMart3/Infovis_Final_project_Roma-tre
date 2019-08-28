007970a95a3766cb678add7844a6da4a9eabadf9
AleMart3 <alex.3@live.it>
2019-08-28 16:49:26 +0200

    Update run-application.sh

--- run-application.sh
+++ run-application.sh
@@ -2,12 +2,20 @@
 
 echo Starting the application...
 
+echo Avvio zookeeper e kafka
 docker-compose build
 docker-compose up -d zookeeper kafka
+echo wait 20 seconds
 sleep 20
 
-
+echo avvio pagine html e javascript_server_side
 docker-compose up -d html_pages javascript_consumer
+
+echo wait 20 seconds
 sleep 20
+
+echo apertura pagine html
 start chrome http://localhost:8081/apri_pagine.html
+
+echo Avvio producer e media_stelle
 docker-compose up
