6b86204058125e6ef6fcb0c2295736cdf3a078b6
AleMart3 <alex.3@live.it>
2019-08-28 16:50:30 +0200

    Update run-application.sh

--- run-application.sh
+++ run-application.sh
@@ -10,12 +10,11 @@ sleep 20
 
 echo avvio pagine html e javascript_server_side
 docker-compose up -d html_pages javascript_consumer
-
 echo wait 20 seconds
 sleep 20
 
 echo apertura pagine html
 start chrome http://localhost:8081/apri_pagine.html
 
-echo Avvio producer e media_stelle
+echo Avvio analisi streaming
 docker-compose up
